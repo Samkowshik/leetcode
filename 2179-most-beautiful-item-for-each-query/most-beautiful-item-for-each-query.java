997928c4a4ffcc2bbf353fbaeb6e065c1630eb6a
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2024-11-12 16:52:34 +0530

    Time: 31 ms (100.00%) | Memory: 81 MB (66.86%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) 
+    {
+        int n = queries.length;
+        int[] ans = new int[n];
+
+        TreeSet<Integer> set = new TreeSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,0);
+        set.add(0);
+
+        for(int[] arr: items)
+        {
+            int minval = map.get(set.floor(arr[0]));
+            if(minval < arr[1])
+            {
+                map.put(arr[0],arr[1]);
+                set.add(arr[0]);
+            }
+        }
+
+        int max = -1;
+        for(Integer i: set)
+        {
+            max = Math.max(max, map.get(i));
+            map.put(i,max);
+        }
+
+        for(int i=0; i<queries.length; i++)
+            queries[i] = map.get(set.floor(queries[i]));
+            
+
+        return queries;
+    }
+}
