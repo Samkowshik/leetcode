8ea8ee96b5aa647b5c913103893f4d78847c5ca2
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2025-03-24 20:58:42 +0530

    Time: 37 ms (78.45%) | Memory: 100.3 MB (66.67%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.java
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -1,19 +1,9 @@
-class MyComp implements Comparator<int[]>
-{
-    public int compare(int[] a, int[] b)
-    {
-        if(a[0]==b[0] && a[1]>b[1])
-            return 1;
-        if(a[0]>b[0])
-            return 1;
-        return -1;
-    }
-}
+
 class Solution 
 {
     public int countDays(int days, int[][] meetings)
     {
-        Arrays.sort(meetings, new MyComp());
+        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0],b[0]));
         
         int srt = meetings[0][0], end = meetings[0][1];
 
