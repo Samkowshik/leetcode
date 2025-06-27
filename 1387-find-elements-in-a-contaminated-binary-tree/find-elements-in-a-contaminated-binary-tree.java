c2dab8321aea139be09b168fb6632ec734090e87
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2025-06-27 15:30:06 +0530

    Time: 21 ms (64.03%) | Memory: 47.4 MB (59.51%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -15,23 +15,23 @@
  */
 class FindElements 
 {
-    List<Integer> lst;
+    Set<Integer> set;
     public FindElements(TreeNode root) 
     {
-        lst = new ArrayList<>();
+        set = new HashSet<>();
         build(root, 0);
     }
     void build(TreeNode node, int val)
     {
         if(node == null) return;
-        lst.add(val);
+        set.add(val);
         build(node.left, val*2+1);
         build(node.right, val*2+2);
     } 
     
     public boolean find(int target)
     {
-        return lst.contains(target);
+        return set.contains(target);
     }
 }
 
