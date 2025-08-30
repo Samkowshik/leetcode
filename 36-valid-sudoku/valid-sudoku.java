1cb9e1ffcdcbf45912c3a15f9c9d1467c2a5f4cb
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2025-08-30 10:14:13 +0530

    Time: 1 ms (99.87%) | Memory: 44.2 MB (89.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -5,16 +5,16 @@ class Solution
         int x = sud[i][j];
         for(int k=0; k<9; k++)
             if(k!=j && sud[i][k] == x)
-                return false;
-        return true;
+                return true;
+        return false;
     }
     static boolean col(char[][] sud, int i, int j)
     {
         int x = sud[i][j];
         for(int k=0; k<9; k++)
             if(k!=i && sud[k][j] == x)
-                return false;
-        return true;
+                return true;
+        return false;
     }
     static boolean box(char[][] sud, int i, int j)
     {
@@ -22,18 +22,15 @@ class Solution
         for(int k=m; k<m+3; k++)
         for(int l=n; l<n+3; l++)
             if(!(k==i && l==j) && sud[k][l]==x)
-                return false;
-        return true;
+                return true;
+        return false;
     }
     public boolean isValidSudoku(char[][] sud) 
     {
         for(int i=0; i<9; i++)
         for(int j=0; j<9; j++)
-        {
-            if(sud[i][j]=='.') continue;
-            if(!(row(sud,i,j) && col(sud,i,j) && box(sud,i,j)))
+            if(sud[i][j]!='.' && (row(sud,i,j) || col(sud,i,j) || box(sud,i,j)))
                 return false;
-        }
 
         return true;
     }
