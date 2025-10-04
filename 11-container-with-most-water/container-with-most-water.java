8af34df73d037061c2ea7c805cccf87f61b50481
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2025-10-04 19:47:57 +0530

    Time: 5 ms (77.27%) | Memory: 56.9 MB (97.87%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,22 +1,16 @@
 class Solution {
     public int maxArea(int[] nums) 
     {
-        int dup, ans =0, n= nums.length, i=0, j = n-1;
+        int ans =0, n= nums.length, i=0, j = n-1;
         while(i<j)
         {
             int min = Math.min(nums[i], nums[j]);
             ans = Math.max(ans, min*(j-i));
 
             if(nums[i] <= nums[j])
-            {
-                dup = nums[i];
-                while(i<n && nums[i] <= dup) i++;
-            }
+                i++;
             else
-            {
-                dup = nums[j];
-                while(j>0 && nums[j] <= dup) j--;
-            }
+                j--;
         }
         return ans;
     }
